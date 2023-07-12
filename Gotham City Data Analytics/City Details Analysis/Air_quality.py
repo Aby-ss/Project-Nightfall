61149e643e21b67d876b4ff67d54592ef120f2d7
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-13 01:31:33 +0400

    Added libraries for customisability ✅

--- Gotham City Data Analytics/City Details Analysis/Air_quality.py
+++ Gotham City Data Analytics/City Details Analysis/Air_quality.py
@@ -1,4 +1,30 @@
+from datetime import datetime
+import csv
+import time
+from time import sleep
+import math
+import keyboard
+import numpy as np
+import asciichartpy
 import requests
+
+from rich import print
+from rich import box
+from rich.tree import Tree
+from rich.text import Text
+from rich.align import Align
+from rich.panel import Panel
+from rich.layout import Layout 
+from rich.table import Table
+
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.progress import track
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
+
+from rich.traceback import install
+install(show_locals=True)
+
 
 city = 'london'
 api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
