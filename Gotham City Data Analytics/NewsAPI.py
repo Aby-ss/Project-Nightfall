e9106c9f9fc0c6a115414340e416666f6b8a5a6e
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-12 16:21:18 +0400

    Added more libraries for customisation ✅

--- Gotham City Data Analytics/NewsAPI.py
+++ Gotham City Data Analytics/NewsAPI.py
@@ -1,4 +1,29 @@
+from datetime import datetime
+import csv
+import time
+from time import sleep
+import math
+import keyboard
+import numpy as np
+import asciichartpy
 import requests
+
+from rich import print
+from rich import box
+from rich.tree import Tree
+from rich.text import Text
+from rich.align import Align
+from rich.panel import Panel
+from rich.layout import Layout 
+from rich.table import Table
+
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.progress import track
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
+
+from rich.traceback import install
+install(show_locals=True)
 
 # Replace 'YOUR_API_KEY' with your actual API key from NewsAPI
 api_key = 'd88f50af7b21490889c52621330e9a2c'
@@ -27,5 +52,7 @@ if response.status_code == 200:
         print(f"Description: {description}")
         print(f"Source: {source}")
         print("---")
+        
+        print(Panel())
 else:
     print(f"Error: {response.status_code}")
