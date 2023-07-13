d5bf894afa90c3f711855cd20e7279ff06717a26
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-13 14:42:23 +0400

    Added Reverse Geocodoung API KEY ✅

--- Gotham City Data Analytics/City Details Analysis/Reverse_Geocoding_API.py
+++ Gotham City Data Analytics/City Details Analysis/Reverse_Geocoding_API.py
@@ -4,7 +4,7 @@ lat = 51.509865
 lon = -0.118092
 api_url = 'https://api.api-ninjas.com/v1/reversegeocoding?lat={}&lon={}'.format(lat, lon)
 
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+response = requests.get(api_url, headers={'X-Api-Key': 'E9SmCxTQQtyVD79D96wfpg==7Z30WK1D8Do2AOcN'})
 if response.status_code == requests.codes.ok:
     data = response.json()
     for location in data:
