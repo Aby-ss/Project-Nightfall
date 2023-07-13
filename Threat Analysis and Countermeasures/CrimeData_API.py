d12411fe4a56b4642db083396654facf5abed8cf
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-14 01:43:59 +0400

    Added CrimeData API base code ✅

---
+++ Threat Analysis and Countermeasures/CrimeData_API.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://jgentes-crime-data-v1.p.rapidapi.com/crime"
+
+querystring = {"startdate":"9/19/2015","enddate":"9/25/2015","long":"-122.5076392","lat":"37.757815"}
+
+headers = {
+	"X-RapidAPI-Key": "3c07ea8d1bmsh488a9163b679e0ep1233d4jsnfe1622183af4",
+	"X-RapidAPI-Host": "jgentes-Crime-Data-v1.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
