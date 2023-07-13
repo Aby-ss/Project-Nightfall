4240266d31b67d7d124af0f3d542d691198c6672
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-14 01:45:05 +0400

    Added UK Police Data API base code ✅

---
+++ Threat Analysis and Countermeasures/UKPoliceData_API.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://ukpolicedata.p.rapidapi.com/%7Bforce-id%7D/%7Bneighbourhood-id%7D/events"
+
+headers = {
+	"X-RapidAPI-Key": "3c07ea8d1bmsh488a9163b679e0ep1233d4jsnfe1622183af4",
+	"X-RapidAPI-Host": "ukpolicedata.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
