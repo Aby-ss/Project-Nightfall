b3683ad667a3b960a1ce7b9a3466170b8ca4b0a2
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-14 13:00:35 +0400

    Updated URL Scanner code for APIKEY and URL search Header ✅

--- Threat Analysis and Countermeasures/VirusTotal/ScanURL.py
+++ Threat Analysis and Countermeasures/VirusTotal/ScanURL.py
@@ -2,11 +2,13 @@ import requests
 
 url = "https://www.virustotal.com/api/v3/urls"
 
+payload = { "url": "https://github.com/Aby-ss" }
 headers = {
     "accept": "application/json",
+    "x-apikey": "41033a47cf93b08bb54269ba13a31b85f239399ac46f00d0e4b3559931de22d3",
     "content-type": "application/x-www-form-urlencoded"
 }
 
-response = requests.post(url, headers=headers)
+response = requests.post(url, data=payload, headers=headers)
 
 print(response.text)
