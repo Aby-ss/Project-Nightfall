4f6c9af72191b7dbe112e1a0b868c1686e50f9c8
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-16 15:13:28 +0400

    Added custom lib ✅

--- Gotham City Data Analytics/environ_factors.py
+++ Gotham City Data Analytics/environ_factors.py
@@ -1,4 +1,29 @@
+from datetime import datetime
+import csv
+import time
+from time import sleep
+import math
+import keyboard
+import numpy as np
+import asciichartpy
 import requests
+
+from rich import print
+from rich import box
+from rich.tree import Tree
+from rich.text import Text
+from rich.align import Align
+from rich.panel import Panel
+from rich.layout import Layout 
+from rich.table import Table
+
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.progress import track
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
+
+from rich.traceback import install
+install(show_locals=True)
 
 def get_air_quality_data(location):
     base_url = "https://api.openaq.org/v1/measurements"
